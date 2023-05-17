af373e2cd6c67d61ae3722b511a76b2d0b932c8c
Niels gilon <gilonniels@gmail.com>
2023-05-17 15:29:10 +0200

    Variabelen aangepast, minder comments
    Je ziet dat ik de variabelen voor de csv info  heb aangepast omdat ik deze makkelijker
    vind om mee te werken, ik heb veel comments verwijderd voor performantie
    redenen en overzichtelijkheid. er zijn ook enkele modules bijgekomen
    omdat ik in de testversie van het script enkele gebruik voor
    troubleshooting doeleinden. verder zet ik alles in het script ook in
    het Engels ipv engels en Nederlands, dit werd te chaotisch.

--- Python/examenopdracht.py
+++ Python/examenopdracht.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python3
 
-# aanmaken van argument parser voor de optie -c | --create
-import argparse #importeer module argparse om gebruik te maken van zijn functies
-import sys #importeer module sys om gebruik te maken van zijn functies
+import argparse #import argparse module for the command arguments
+import csv #import csv module to read and extract info from csv file
+import os #import os module to make use of its path functions
+import subprocess #import subprocces module to make use off bash commands in the script
+import sys #import sys module to use for troubleshooting purposes
+import crypt #import crypt module to encrypt the plain text passwds from the csv file
 
-import csv #importeer module csv om csv files in te lezen mogelijk te maken
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Create users from a CSV file.')
+parser.add_argument('-c', '--create', metavar='csv_file', type=str,
+                    help='Create users from the specified CSV file')
+args = parser.parse_args()
 
-parser = argparse.ArgumentParser(description='maak gebruikers aan van een CSV file.') #maak de argument parser aan
-parser.add_argument('-c', '--create', metavar='csv file', type=str,                   #voeg argument -c en --create toe plus metavar --> dit om de help statement meer leesbaar te maken bv -c csv file en --create csv file
-                    help='maak gebruikers aan van de gespecifieerde CSV file')        #description voor help statement
-args = parser.parse_args()                                                            #parser oproepen voor gebruik
+if not args.create:
+    print("Please specify a CSV file using the -c or --create option.")
+    sys.exit(1)
 
-if not args.create:                                                                   #argument gebruiken in if statement
-    print("Specifieer alstublieft een csv file na de optie -c of --create.")          #als het argument oningevuld blijft print deze output op de terminal
-    sys.exit(1)                                                                       #exit code 1 om te indiceren dat een error is opgetreden als de plaats van het argument leeg blijft
+# Read the CSV file
+with open(args.create, newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
 
-# lees de csv file in
-with open(args.create, newline='') as csvfile:                                        #def open om gespecifieerde file te openen geeft nieuwe variabele csvfile ipv args.creat                                                       
-    reader = csv.DictReader(csvfile, delimiter=';')                                   #csv.dictreader van variabele csvfile als nieuwe variabele 'reader' om door te loopen
-                                                                                      #belangrijk! delimiter als ';' ingesteld = teken dat values separate in mijn csv file
-    for row in reader:                                                                #door de file loopen
-        # gebruikers informatie van de csv file ophalen en word in variabele gestoken om later te gebruiken
-        gebruikersnaam = row['studentid']                                             #per iteratie in csv bestand 4 variabelen aanmaaken
-        wachtwoord = row['wachtwoord']                                                
-        groepnaam = row['klasgroep']
+
+    # Check if the class group exists, and create it if it doesn't
+
+    try:
+     subprocess.run(['getent', 'group', 'students'], check=True)
+    except subprocess.CalledProcessError:
+            subprocess.run(['groupadd', 'students'], check=True)
+
+    for row in reader:
+        # Extract the username and group name from the CSV file and stor in variables 
+        username = row['studentid']
+        password = row['wachtwoord']
+        groupname = row['klasgroep']
         public_key = row['public_key']
         email = row['email']
-        home_dir = '/home/' + email.split('@')[0]                            #de home directory moest /home/"alles voor het @ teken in email adres zijn"
+        home_dir = '/home/' + email.split('@')[0]
+
+        print(f"public_key: '{public_key}'")
