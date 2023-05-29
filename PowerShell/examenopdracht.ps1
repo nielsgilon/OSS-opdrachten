958f462d324695e081130b60380e9e99399afbb6
Niels Gilon <niels.gilon@student.ap.be>
2023-05-29 18:56:53 +0200

    succesvol geconnect beide cloud/server
    Bij deze heb ik succesvol een connectie kunnen maken met zowel mijn eigen AD server als de azure ad omgeving van de school

--- PowerShell/examenopdracht.ps1
+++ PowerShell/examenopdracht.ps1
@@ -1,2 +1,7 @@
 Clear-Host
 
+# Connect to the remote AD server
+New-PSSession -ComputerName PFSV1NG -UseSSL -Credential (Get-Credential)
+
+# Connect to the AzureAD cloud environment
+Connect-AzureAD
